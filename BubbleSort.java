7a6e416664359d4c3a8f42501cb57efbf96aab11
eRues <ellie.rue@stu.daviess.kyschools.us>
2025-04-18 04:42:16 +0000

    finished

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,9 +4,24 @@ public class BubbleSort
 	public static void main(String[] args)
   {
     int[] myNums = {12, 15, 0, 44, 13, 1, 2};
-		int passes;
-		int comparisons;
+		int passes = myNums.length;
+		int comparisons = myNums.length - 1;
 		int tempNum;
+
+		for(int i = passes; i < passes; i++)
+		{
+			for(int j = 0 ; j < comparisons; j++)
+			{
+				if(myNums[j] > myNums[j + 1])
+				{
+					tempNum = myNums[j];
+					myNums[j] = myNums[j + 1];
+					myNums[j + 1] = tempNum;
+				}
+			}
+			comparisons--;
+		}
+
 
 		//Loop once for each pass, where passes is one less than the number of items.
 			//Loop once for each comparison, where comparisons are one less than the number of unsorted.
